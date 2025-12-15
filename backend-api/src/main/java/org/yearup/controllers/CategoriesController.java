da6f8e4efa78b5cd5e6b7bc4e292a5aac2fecba1
Bethlehem <aberabetty3@gmail.com>
2025-12-14 20:57:31 -0500

    Implement Categories GET endpoint with MySQL DAO Implement product-by-category endpoint

--- backend-api/src/main/java/org/yearup/controllers/CategoriesController.java
+++ backend-api/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -39,10 +39,11 @@ public class CategoriesController
     }
 
     // add the appropriate annotation for a get action
+    @GetMapping("/id")
     public Category getById(@PathVariable int id)
     {
         // get the category by id
-        return null;
+        return categoryDao.getById(id);
     }
 
     // the url to return all products in category 1 would look like this
@@ -51,7 +52,7 @@ public class CategoriesController
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
-        return null;
+        return productDao.listByCategoryId(categoryId);
     }
 
     // add annotation to call this method for a POST action
