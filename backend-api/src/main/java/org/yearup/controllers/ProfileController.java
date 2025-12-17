0e08d242b044fd122d305f105823d806a1241797
Bethlehem <aberabetty3@gmail.com>
2025-12-17 18:36:24 -0500

    Implement Categories GET endpoint with MySQL DAO Implement product-by-category endpoint Added GET /categories/{id} product endpoint Phase 1 completed: categories,products, auto, security Phase 2 -Added ShoppingCartController endpoints and DAO stubs start phase 3 cart updates setup fixed put cart endpoint to use request body
    Implement shopping cart add product flow, DAO integration, and JWT-secured endpoints
    Added product to cart endpoint with auto and response
    
    Phase 3:
    Added POST/orders endpoint secured with JWT
    Implemented OrderDao and MysqlOrderDao createOrder Logic
    Inserted orders and order_line_items using INSERT SELECT
    Linked orders to authenticated user via Principal
    Cleared shopping cart after successful checkout
    Fixed category delete and return 404 when category not found
    added update and delete methode
    JWT login returns token
    Added get profile endpoint for authenticated user
    added update profile endpoint
    implement profile DAO and MySQL DAO

---
+++ backend-api/src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,49 @@
+package org.yearup.controllers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.data.ProfileDao;
+import org.yearup.models.Profile;
+import org.yearup.data.UserDao;
+import org.yearup.models.User;
+
+@RestController
+@RequestMapping("/profile")
+@PreAuthorize("isAuthenticated")
+
+public class ProfileController {
+    private final ProfileDao profileDao;
+    private final UserDao userDao;
+
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
+@GetMapping
+public  ResponseEntity<Profile> getProfile(Authentication authentication) {
+        String username = authentication.getName();
+        int userId = userDao.getIdByUsername(username);
+    Profile profile = profileDao.getByUserId(userId);
+
+    if (profile == null) {
+        return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(profile);
+    }
+@PutMapping
+public ResponseEntity<Profile> updateProfile(Authentication authentication, @RequestBody Profile profile)
+    {
+        String username = authentication.getName();
+        int userId = userDao.getIdByUsername(username);
+        profile.setUserId(userId);
+        profileDao.update(profile);
+
+        Profile updated = profileDao.getByUserId(userId);
+        return ResponseEntity.ok(updated);
+
+    }
+}
+
+
--- backend-api/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ backend-api/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -1,5 +1,6 @@
 package org.yearup.data.mysql;
 
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
 import org.yearup.models.Profile;
 import org.yearup.data.ProfileDao;
@@ -8,21 +9,17 @@ import javax.sql.DataSource;
 import java.sql.*;
 
 @Component
-public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
-{
-    public MySqlProfileDao(DataSource dataSource)
-    {
+public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao {
+    public MySqlProfileDao(DataSource dataSource) {
         super(dataSource);
     }
 
     @Override
-    public Profile create(Profile profile)
-    {
+    public Profile create(Profile profile) {
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
                 " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        try(Connection connection = getConnection())
-        {
+        try (Connection connection = getConnection()) {
             PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
             ps.setInt(1, profile.getUserId());
             ps.setString(2, profile.getFirstName());
@@ -37,11 +34,47 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
             ps.executeUpdate();
 
             return profile;
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+    @Override
+    public Profile getByUserId(int userId) {
+        String sql = """
+                SELECT user_id, first_name, last_name, phone, email, address, city, state, zip
+                FROM profiles
+                WHERE user_id = ?""";
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, userId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Profile profile = new Profile();
+                profile.setUserId(resultSet.getInt("user_id"));
+                profile.setFirstName(resultSet.getString("first_name"));
+                profile.setLastName(resultSet.getString("last_name"));
+                profile.setPhone(resultSet.getString("phone"));
+                profile.setEmail(resultSet.getString("email"));
+                profile.setAddress(resultSet.getString("address"));
+                profile.setCity(resultSet.getString("city"));
+                profile.setState(resultSet.getString("state"));
+                profile.setZip(resultSet.getString("zip"));
+                return profile;
+            }
+
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    public void update(Profile profile) {
+
+    }
 }
+
+
+
+
